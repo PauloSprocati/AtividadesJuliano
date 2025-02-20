772816dc1ae4c9ca497f0c5f95263e2c82f196e6
leicam <julianoleicam@gmail.com>
2025-02-20 12:31:51 -0300

    feat: projeto inicial atividades

---
+++ ProgramacaoIV.Exercicios/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ProgramacaoIV.Exercicios.Janelas;
+using System.Windows;
+
+namespace ProgramacaoIV.Exercicios;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void btnExercicioUm_Click(object sender, RoutedEventArgs e)
+    {
+        new ExercicioUm().ShowDialog();
+    }
+}
